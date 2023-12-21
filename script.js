0ae58bd6e5659f9628d462e906474c5a2ccc4a54
odaya <odayanagosa781@icloud.com>
2023-12-21 21:12:30 +0200

    week3 day5

---
+++ script.js
@@ -0,0 +1,30 @@
+let mydata = {
+name: 'nadav sofer',
+age: 25,
+}
+mydata.eyecolor = 'green' 
+console.log(mydata);
+
+let userdata = {
+    userpassword
+}
+let myage = 2
+if (myage >= 21 ){
+console.log('nadav can drink,not in the us');
+}
+else {
+    console.log ('password is too short'); 
+} 
+
+let password = 'nadav1998';
+
+if (password.length > 8) {
+console.log('password is ok');
+} else {
+    console.log('password is too short')
+}
+
+
+
+
+
---
+++ week 3/day 1/script.js
@@ -0,0 +1,29 @@
+let thing='hello world'
+const thing2 =4
+console.log(thing);
+
+let a = '5'
+let b = 9
+let c= 5+20
+
+console.log(a+b)
+console.log(a+c);
+
+console.log(a*c+(b+a-1));
+
+
+
+let string= 'look it'
+let name = 'value'
+let str2 = 'hollo its me '
+let str3 = hello 
+let users {'mike',steve'greg'}
+users.push('sarah')
+users.pop()
+users.splice(1,0,'yafa,'odaya')
+let first3= users.slice(0,3)
+console.log(users);
+console.log(first3);
+
+
+
